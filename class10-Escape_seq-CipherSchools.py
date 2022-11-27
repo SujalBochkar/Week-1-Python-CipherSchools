52b29c51b34e7749bcc2277aa1be10719e530d5d
SujalBochkar <sujalbochkar@gmail.com>
2022-11-28 00:15:53 +0530

    Initial commit

---
+++ class10-Escape_seq-CipherSchools.py
@@ -0,0 +1,16 @@
+print("hello \"world\"world")
+print('I\'m Sujal Bochkar')
+print("hii \nhello")
+print("hii \tbye")
+print("good bye\\")
+print("this is double blackslash\\\\")
+print("hii \bhello")
+
+
+
+# \' Single quote
+# \" Double Quote
+# \\ blackslash
+# \n new line
+# \t tab
+# \b Double Quote
